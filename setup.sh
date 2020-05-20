9733b8cf206f5e49db606a3d0f577e7d09cc1dd8
Ben Whitley <ben@touchlab.co>
2020-05-20 12:00:55 -0400

    updated script to so plugin can be reloaded after an xcode update

--- setup.sh
+++ setup.sh
@@ -1,15 +1,39 @@
 #!/usr/bin/env bash
 
 ###################
-# PLUGINS DIRECTORY
+# DEFINITIONS
 ###################
 
+service='Xcode'
 plugins_dir=~/Library/Developer/Xcode/Plug-ins/
 
-if [ ! -d "$plugins_dir" ]; then
-	mkdir -p $plugins_dir
+###################
+# SHUT DOWN XCODE IF IT'S RUNNING
+###################
+
+if pgrep -xq -- "${service}"; then
+  echo "Xcode is running. Attempt to shut down?"
+  read answer
+  if [ "$answer" = "y" ]; then
+    echo "Shutting down Xcode"
+    pkill -x $service
+  else
+    echo "Xcode needs to be closed"
+    exit 1
+  fi
 fi
 
+###################
+
+if [ -d "${plugins_dir}Kotlin.ideplugin/" ]; then
+	echo "Plugins directory and Kotlin plugin found. Deleting..."
+  rm -rf $plugins_dir
+  open -a $service
+  pkill -x $service
+fi
+
+echo "Creating plugins directory"
+mkdir -p $plugins_dir
 cp -r Kotlin.ideplugin $plugins_dir
 
 ###################
@@ -28,3 +52,4 @@ else
 		echo $lldb_config >> ~/.lldbinit-Xcode
 		echo $lldb_format >> ~/.lldbinit-Xcode
 fi
+
