db519578750dd418a6a39c0a775660178f6fbaf1
Alex Trotsenko <olexii.trotsenko@gmail.com>
2020-11-02 13:12:24 +0100

    Use current default Xcode path instead of hard-coded Xcode path
    Required when you have multiple Xcode installations

--- colorsetup-xcode11.sh
+++ colorsetup-xcode11.sh
@@ -4,10 +4,15 @@
 # LANGUAGE SPEC
 ###################
 
-spec_dir=/Applications/Xcode.app/Contents/SharedFrameworks/SourceModel.framework/Versions/A/Resources/LanguageSpecifications
+xcode_developer_dir=$(xcode-select --print-path)
+
+## Default is /Applications/Xcode.app/Contents
+xcode_content_dir=$(dirname "$xcode_developer_dir")
+
+spec_dir=$xcode_content_dir/SharedFrameworks/SourceModel.framework/Versions/A/Resources/LanguageSpecifications
 
 cp Kotlin.xclangspec $spec_dir
 
-meta_dir=/Applications/Xcode.app/Contents/SharedFrameworks/SourceModel.framework/Versions/A/Resources/LanguageMetadata
+meta_dir=$xcode_content_dir/SharedFrameworks/SourceModel.framework/Versions/A/Resources/LanguageMetadata
 
 cp Xcode.SourceCodeLanguage.Kotlin.plist $meta_dir
