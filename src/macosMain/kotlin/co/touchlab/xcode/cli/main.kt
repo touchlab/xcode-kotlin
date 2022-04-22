50e63176c14d45feca4a65b1a110cbfce10c59b1
Tadeas Kriz <tadeas@touchlab.co>
2022-04-22 14:03:09 -0400

    Show usage if no arguments are passed in.

--- src/macosMain/kotlin/co/touchlab/xcode/cli/main.kt
+++ src/macosMain/kotlin/co/touchlab/xcode/cli/main.kt
@@ -9,6 +9,12 @@ import co.touchlab.xcode.cli.command.Uninstall
 import kotlinx.cli.ArgParser
 
 fun main(args: Array<String>) {
+    // If no arguments were given, we want to show help.
+    val adjustedArgs = if (args.isEmpty()) {
+        arrayOf("-h")
+    } else {
+        args
+    }
     Logger.setLogWriters(platformLogWriter())
     val parser = ArgParser("xcode-kotlin")
     parser.subcommands(
@@ -18,5 +24,5 @@ fun main(args: Array<String>) {
         Info(),
     )
 
-    parser.parse(args)
+    parser.parse(adjustedArgs)
 }
