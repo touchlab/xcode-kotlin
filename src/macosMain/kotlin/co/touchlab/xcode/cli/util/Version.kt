534c05edbcf44597db662fdac3602cfc41007351
Tadeas Kriz <tadeas@touchlab.co>
2022-04-28 15:55:11 -0400

    Add missing crash when parsed version doesn't match.

--- src/macosMain/kotlin/co/touchlab/xcode/cli/util/Version.kt
+++ src/macosMain/kotlin/co/touchlab/xcode/cli/util/Version.kt
@@ -4,7 +4,7 @@ typealias Version = KotlinVersion
 
 private val versionRegex = Regex("(\\d+)(?:\\.(\\d+)(?:\\.(\\d+))?)?")
 fun KotlinVersion.Companion.fromString(version: String): KotlinVersion {
-    val result = versionRegex.matchEntire(version) ?: TODO()
+    val result = requireNotNull(versionRegex.matchEntire(version)) { "Version $version can't be parsed!" }
     val major = result.groupValues[1].toInt()
     val minor = result.groups[2]?.value?.toInt() ?: 0
     val patch = result.groups[3]?.value?.toInt() ?: 0
