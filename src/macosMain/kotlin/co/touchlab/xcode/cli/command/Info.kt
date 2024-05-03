22c40a7ea5e90c57b431340bd56b70cf1c492eb7
Kevin Galligan <kgalligan@gmail.com>
2024-05-03 11:12:36 -0400

    Fix info command for 15.3

--- src/macosMain/kotlin/co/touchlab/xcode/cli/command/Info.kt
+++ src/macosMain/kotlin/co/touchlab/xcode/cli/command/Info.kt
@@ -43,8 +43,9 @@ class Info: BaseXcodeListSubcommand("info", "Shows information about the plugin"
             echo("Installed Xcode versions:")
             val longestNameLength = xcodeInstallations.maxOf { it.name.length }
             for (install in xcodeInstallations) {
-                val spacesAfterName = (1..(longestNameLength - install.name.length)).joinToString(separator = "") { " " }
-                val compatibilityMark = if (supportedXcodeUuids.contains(install.pluginCompatabilityId)) "✔" else "x"
+                val spacesAfterName =
+                    (1..(longestNameLength - install.name.length)).joinToString(separator = "") { " " }
+                val compatibilityMark = if (install.supported(supportedXcodeUuids)) "✔" else "x"
                 echo("$compatibilityMark\t${install.name}$spacesAfterName\t${install.pluginCompatabilityId}\t${install.path}")
             }
 
